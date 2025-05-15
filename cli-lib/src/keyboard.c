32cc4f1282f8ad34d7a2095ad0e0897d295c3658
Thyalles <thyalles1@gmail.com>
2025-05-14 21:57:08 -0300

    Ajustando arquios

--- cli-lib/src/keyboard.c
+++ cli-lib/src/keyboard.c
@@ -1,68 +1,23 @@
 /**
- * keyboard.h
- * Created on Aug, 23th 2023
- * Author: Tiago Barros
- * Based on "From C to C++ course - 2002"
-*/
+ * keyboard.c
+ * Adaptado para Windows
+ */
 
-#include <termios.h>
-#include <unistd.h>
-
+#include <conio.h>
 #include "keyboard.h"
 
-static struct termios initialSettings, newSettings;
-static int peekCharacter;
-
-
-void keyboardInit()
-{
-    tcgetattr(0,&initialSettings);
-    newSettings = initialSettings;
-    newSettings.c_lflag &= ~ICANON;
-    newSettings.c_lflag &= ~ECHO;
-    newSettings.c_lflag &= ~ISIG;
-    newSettings.c_cc[VMIN] = 1;
-    newSettings.c_cc[VTIME] = 0;
-    tcsetattr(0, TCSANOW, &newSettings);
+void keyboardInit() {
+    // Não é necessário nada no Windows
 }
 
-void keyboardDestroy()
-{
-    tcsetattr(0, TCSANOW, &initialSettings);
+void keyboardDestroy() {
+    // Também não é necessário nada no Windows
 }
 
-int keyhit()
-{
-    unsigned char ch;
-    int nread;
-
-    if (peekCharacter != -1) return 1;
-    
-    newSettings.c_cc[VMIN]=0;
-    tcsetattr(0, TCSANOW, &newSettings);
-    nread = read(0,&ch,1);
-    newSettings.c_cc[VMIN]=1;
-    tcsetattr(0, TCSANOW, &newSettings);
-    
-    if(nread == 1) 
-    {
-        peekCharacter = ch;
-        return 1;
-    }
-    
-    return 0;
+int keyhit() {
+    return _kbhit(); // verifica se alguma tecla foi pressionada
 }
 
-int readch()
-{
-    char ch;
-
-    if(peekCharacter != -1)
-    {
-        ch = peekCharacter;
-        peekCharacter = -1;
-        return ch;
-    }
-    read(0,&ch,1);
-    return ch;
+int readch() {
+    return _getch(); // lê a tecla pressionada
 }
